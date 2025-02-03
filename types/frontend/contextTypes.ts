1d12fc239bdba157e1031af65fa811406bf389e4
Pushkar Dev <pushkardev54321@gmail.com>
2025-02-04 00:17:01 +0530

    Modified Static data imports

---
+++ types/frontend/contextTypes.ts
@@ -0,0 +1,48 @@
+import Permission from "../permissions";
+import { AxiosError } from "axios";
+
+export type RequiredPermissions = Permission[] | null;
+export type CustomError = AxiosError | Error | null;
+export type GrantedPermissions = Permission[] | "*" | null;
+
+export type CurrentUserCtxType = {
+  grantedPermissions: GrantedPermissions;
+  user: IUser;
+  setGrantedPermissions: (permissions: GrantedPermissions) => void;
+  setUser: (user: IUser) => void;
+  ready: boolean;
+  setReady: () => void;
+};
+
+export type ErrorCtxType = {
+  setError: (error: CustomError) => void;
+};
+
+export type PermErrCtxType = {
+  failedPermissions: RequiredPermissions;
+  setFailedPermissions: (error: RequiredPermissions) => void;
+};
+
+export interface IUser {
+  name: string;
+  email: string;
+  id: string;
+  bio: string;
+  role: string;
+  permission: Permission[];
+  is_superuser: boolean;
+  created_at: string;
+  team_role: MainWebsiteRole;
+}
+
+export enum MainWebsiteRole {
+  DoNotDisplay,
+  Advisor,
+  Coordinator,
+  Columnist,
+  Designer,
+  Photographer,
+  Illustrator,
+  Developer,
+  Alumni,
+}
